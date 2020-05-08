ba4649a8d2945234f3a52a006979f3f068acc875
gdwLandSurveys <gdwLandSurveys@perth.regis.local>
2020-05-08 12:03:06 +0800

    Readme & GET

--- README.md
+++ README.md
@@ -8,13 +8,28 @@ Include screenshots for editing alias and translation files within Surpac for ea
 # Alby Surpac
 
 ## About
-In brief, this is a repo for alby's surpac files. It contains items from styles and profiles through to drawing files and macros.
+In brief, this is a repo for Alby's Surpac files. It contains items from styles and profiles through to drawing files and macros.
 - Includes macros, styles, profiles and templates to be used within Surpac
 - Up to date files for use with latest versions of Surpac
 >**Note:** Majority of macros are designed for with absolute references for logicals and files
 
+
+
+<!--
+Create a wiki website that can be accessed with information about Surpac and the macros that have been created.
+Provide direct download links - possibly include a "total downloads" statistic.
+
+Could potentially use the inbuilt GitHub Wiki for this
+
+## Wiki
+
+
+
+-->
+
 ## Important Info
 - Surpac default files should only be changed as an advanced user
+- Modifying default files or directing towards new files is likely to cause issues if your administrator / supervisor has set up Surpac specifically for your working environment.
 
 ## Macros
 Macros can be used as a tool to perform multiple tasks whether they be simple or intricate.
@@ -27,9 +42,10 @@ Processing a blasted stock volume
 - generate a custom volume and hole report for easy transposing into spreadsheets
 - update blast boundary strings for record keeping and production assistance
 
-### currentpit_contours
+### Currentpit Contours
 Launching the currentpit_contours macro opens the following GUI in Surpac
-[![CPC](Source/Media/M_CPC.png)]
+![CPC](Source/Media/M_CPC.png)
+
 As you can see, the macro allows you to select a "pit", this step essentially sets the working directory to that of the pit.
 The macro then has options to decide the contour intervals to generate (that of which can be changed within the macro.tcl to suit requirements).
 Also allowing the generation of a DTM (Digital Terrain Model) for enhanced visuals.
@@ -41,4 +57,4 @@ This macro is ready to use within any environment as it has no absolute referenc
 The general tasks of the macro include:
 - user options to import "point cloud", "dtm" or "dxf mesh" files
 - user defined export name and location
-- 
+-
--- Working/get processing/get_new.tcl
+++ Working/get processing/get_new.tcl
@@ -24,41 +24,41 @@ set myForm {
     -defaults_key sectionsfile
     -layout BoxLayout Y_AXIS
     -width 40
-    
+
     GuidoPanel panel_left {
       -layout BoxLayout Y_AXIS
-      
+
       # Title
       GuidoLabel bold_label {
       -label "<html><h1>Process GET Strings from Drill Trace</h1><br>
       <font color=\"red\">Set the work directory to the correct folder before proceeding</html>"
       -font_highlight 1 1 1
       }
-      
+
       # Has the Trace Contour been made
-      
+
       # Input File
       GuidoPanel panel_1 {
         -label "Inputs"
         -layout CentreLineLayout
         -border etched true
         -width 30
-        
+
         GuidoButtonGroupPanel noe {
           -label "GET Trace"
-          
+
           GuidoRadioButton noea {
             -caption "New"
             -selected_value "0"
           }
-          
+
           GuidoRadioButton noeb {
             -caption "Existing"
             -selected_value "1"
           }
         }
-        
-		
+
+
         GuidoFileBrowserField traceinput {
           -label "GET trace file"
           -format none
@@ -67,7 +67,7 @@ set myForm {
           -extension false
           -dependency {"[$noea getCurrentValue]" == "0"}
         }
-		
+
         GuidoFileBrowserField contourinput {
           -label "Trace contour file"
           -format none
@@ -77,20 +77,20 @@ set myForm {
           -dependency {"[$noeb getCurrentValue]" == "1"}
         }
       }
-      
+
       GuidoPanel panel_2 {
         -label "Trace settings"
         -layout CentreLineLayout
         -border etched true
         -width 30
-        
+
         GuidoField getlabel {
           -label "GET Label"
           -display_length 25
           -format none
           -null false
         }
-        
+
         GuidoField flitchrng {
           -label "Flitch interval"
           -format integer
@@ -98,44 +98,44 @@ set myForm {
           -default 2.5
           -dependency {"[$noea getCurrentValue]" == "0"}
         }
-        
+
         GuidoButtonGroupPanel profdia {
           -label "GET Radius"
           -dependency {"[$noeb getCurrentValue]" == "1"}
-          
+
           GuidoRadioButton dia {
             -caption "2.5m Radius"
             -selected_value "dia5"
           }
-          
+
           GuidoRadioButton dia {
             -caption "5m Radius"
             -selected_value "dia10"
           }
         }
-        
+
         # Possibly add option to allow for different size profiles to be applied (i.e. 2.5m radius, 5m radius)
       }
-      
+
       GuidoPanel panel_3 {
         -layout CentreLineLayout vertical
         -border etched true
         -width 15
-        
+
         GuidoButtonGroupPanel pit {
           -label "Pit"
           -layout BoxLayout X_AXIS
-        
+
           GuidoRadioButton pita {
             -caption "GW"
             -selected_value "gw"
           }
-          
+
           GuidoRadioButton pitb {
             -caption "TW"
             -selected_value "tw"
           }
-          
+
           GuidoRadioButton pitc {
             -caption "ERL"
             -selected_value "erl"
@@ -157,7 +157,7 @@ if {$_status == "cancel"} then {
 # ##########
 # Constants
 # ##########
-
+set contours _contours
 
 set pathprofile G:10 - Survey/admin/surpac_solid_profiles
 set dia5 G:10 - Survey/admin/surpac_solid_profiles/profile_5m_circle.str
@@ -177,10 +177,76 @@ if {$dia == "dia10"} {
 
 # If new GET trace
 if {$noea == "0"} {
+  set status [ SclFunction "EXIT GRAPHICS" {} ]
+
   set status [ SclFunction "RECALL ANY FILE"  {
     file="$traceinput.str"
     mode="openInNewLayer"
   }]
+
+  set status [ SclFunction "TRIANGULATE CENTRE LINE AND PROFILE" {
+    frm00497={
+      {
+        plloc="profile_5m_circle.str"
+        plid=""
+        use_explicit_object_id="N"
+        scale="1"
+        yoff="0"
+        xoff="0"
+        zoff="0"
+        rotation="0"
+        gfactor="1"
+        vcon="N"
+        triangulate_first_face="N"
+        triangulate_last_face="N"
+      }
+    }
+    select_point={
+      {
+        winx="0"
+        winy="0"
+        objectx="0"
+        objecty="0"
+        objectz="0"
+        snap_projection="off"
+      }
+    }
+  }]
+
+  set status [ SclFunction "FILE SAVE" {
+    frmsaveFileAs={
+      {
+        output_file="delete_me"
+        output_type="Surpac DTM Files"
+        outputExt=".dtm"
+        Surpac={
+          FileFormat="binary"
+          range=""
+          Purpose=""
+          ForceCompatibility="true"
+          ForceValidation="true"
+        }
+        saveStyle="N"
+      }
+    }
+  }]
+
+  set status [ SclFunction "EXTRACT CONTOUR" {
+    frm04949={
+      {
+        dtmloc="delete_me"
+        dtmfld="Z"
+        object_id="1"
+        trisolation_id="2"
+        cloc="$traceinput$contours"
+        contval="N"
+        deftype="R"
+        conrng="0,1000,2.5"
+        index="N"
+      }
+    }
+  }]
+
 }
 
 # If existing GET trace contour
